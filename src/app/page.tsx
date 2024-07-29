84125af4a5c05d981dc121459ef6f648dd32e257
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-30 01:39:03 +0200

    Deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import supabase from "@/app/api/data/route";
+import supabase from '@/lib/supabase';
 import {NextResponse} from "next/server";
 import { useEffect, useState } from 'react';
 
