f9bc0031451716ee5164f6ea62d7868d4c0034b7
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-30 00:40:01 +0200

    Updated api to access foreign table in supabase

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,15 +1,19 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://fsseoukaxwtazmarzdlv.supabase.co';
-const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzc2VvdWtheHd0YXptYXJ6ZGx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyNjg0NzQsImV4cCI6MjAzNzg0NDQ3NH0.xV-VHvM-UaqyJstdukpaRLmFYvhqt3plO5BTNFjk6UQ';
+const SUPABASE_URL = process.env.SUPABASE_URL as string;
+const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY as string;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
 export async function GET() {
-    const { data, error } = await supabase.from('items').select('*');
+
+    let { data, error } = await supabase
+        .from('airtask')
+        .select('*')
 
     if (error) {
+        console.log('Error while connecting to database or while fetching data:', error);
         return NextResponse.json({ error: error.message }, { status: 500 });
     }
     console.log('Connected to database successfully!')
