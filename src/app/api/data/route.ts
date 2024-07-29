2e5224286765f0bb3e9d8908838a46c6eee8e200
VeldinS <veldinsalcinovic123@gmail.com>
2024-07-30 01:20:42 +0200

    Deployment

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.SUPABASE_URL as string;
-const SUPABASE_API_KEY = process.env.SUPABASE_API_KEY as string;
+const SUPABASE_URL = 'https://fsseoukaxwtazmarzdlv.supabase.co';
+const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzc2VvdWtheHd0YXptYXJ6ZGx2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMjI2ODQ3NCwiZXhwIjoyMDM3ODQ0NDc0fQ.u2AVNX-AdiTLAlQFOgM1Kkn1dnuYsjixy6rr2-CU7Mw';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
 
